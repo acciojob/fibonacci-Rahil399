3dc44c4c1b5d0c199fbe9f105a77003144502b8b
Rahil Abdul <153207425+Rahil399@users.noreply.github.com>
2024-11-19 17:57:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function fibonacci(num) {
 // your code here
-	if(num = 0) return 0;
-	if(num===1) return 1
+	if(num === 1) return 0;
+	if(num === 2) return 1
 
 	let a = 0;
 	let b = 1;
 
-	for (let i = 0; i <= num; i++) {
+	for (let i = 3; i <= num; i++) {
 		let temp = a + b;
 		a = b;
 		b = temp;
